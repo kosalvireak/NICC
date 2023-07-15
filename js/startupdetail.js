146c5829c27912a20b098017fe65e97f1216943e
Kosalvireak Khoeun <virakvary@gmail.com>
2023-07-15 13:38:13 +0700

    remove loading

--- js/startupdetail.js
+++ js/startupdetail.js
@@ -15,15 +15,6 @@
     // Initiate the wowjs
     new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
-
 
 
     // Dropdown on mouse hover
